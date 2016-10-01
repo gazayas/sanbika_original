8ca12dea2115543e57e68e9b5d5736836337340a
gazayas <g-zayas@hotmail.com>
2016-10-01 19:30:20 +0900

    songs/index.html.erbを綺麗にしました

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < Devise::SessionsController
   # ログインではメールじゃなくてユーザー名でログインしてほしいのでカスタムのコントローラを作りました
   # http://easyramble.com/cutomize-controllers-on-rails-devise.html
+=begin
   def sign_in(user)
-    session[:id] ||= user.id # 何たらかんたら
+    user = User.find_by_name(name)
+    if user.password == password
+      session[:id] ||= user.id
+      redirect_to user_path
+    else
+      redirect_to root_path
+    end
   end
+=end
+
 end
