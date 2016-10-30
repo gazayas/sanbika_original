e56c5f416aee597db95225d720292128e51531d6
gazayas <g-zayas@hotmail.com>
2016-10-30 11:51:34 +0900

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,19 +81,22 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = {  host: 'sanbika.herokuapp.com' }
-  # mail setting
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV["email_hidden"],
-    :password => ENV["password_hidden"],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+# mail setting
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true # デフォルトは false
+host = 'sanbika.herokuapp.com'
+config.action_mailer.default_url_options = { host: host }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :user_name => ENV["email_hidden"],
+  :password => ENV["password_hidden"],
+  :authentication => :plain,
+  :domain => 'gmail.com'
+  :enable_starttls_auto => true,
+  :openssl_verify_mode => 'none'
+}
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
