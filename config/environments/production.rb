4b9ceaa188f5e4767ee2cb4107ebd265c142553a
gazayas <g-zayas@hotmail.com>
2016-10-30 11:33:34 +0900

    production.rbの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,20 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = {  host: 'sanbika.herokuapp.com' }
+  # mail setting
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV["email_hidden"],
+    :password => ENV["password_hidden"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
