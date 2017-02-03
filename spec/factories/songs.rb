1b207b5a06034dd3ac991477c5dc940045eaa7ab
gazayas <g-zayas@hotmail.com>
2017-02-03 10:02:27 +0900

    song_spec.rbとファクトリの追加

---
+++ spec/factories/songs.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :song do
+    title '歌です'
+    title_yomikata 'うたです'
+    artist '歌手'
+    artist_yomikata 'かしゅ'
+    key 'C'
+    song_body '歌詞はここに入ります'
+  end
+end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Song, type: :model do
 
+  subject { song }
+  let(:user) { FactoryGirl.create(:user) }
+  
+  describe '登録' do
+    context '有効のパラメーター' do
+      let(:song) { FactoryGirl.create(:song, user: user) }
+      it { is_expected.to be_valid }
+    end
+  end
+
 end
