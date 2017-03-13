25aa6017a8302fc7d8067090f5e97d5b2bc14a1b
gazayas <g-zayas@hotmail.com>
2017-03-13 18:40:03 +0900

    deviseのsessions_controllerの修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,6 @@
 class SessionsController < Devise::SessionsController
-  # ログインではメールじゃなくてユーザー名でログインしてほしいのでカスタムのコントローラを作りました
-  # http://easyramble.com/cutomize-controllers-on-rails-devise.html
-=begin
-  def sign_in(user)
-    user = User.find_by_name(name)
-    if user.password == password
-      session[:id] ||= user.id
-      redirect_to user_path
-    else
-      redirect_to root_path
-    end
-  end
-=end
+
+  # 詳しい情報：
+  # https://github.com/plataformatec/devise/blob/master/app/controllers/devise/sessions_controller.rb
 
 end
