754062890d150bd87dfae02a1e09169f313783da
gazayas <g-zayas@hotmail.com>
2018-03-21 08:49:16 +0900

    example_songのロジックのコントローラーの方に移す

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    example_user = User.find_by(name: 'gazayas')
+    @example_song = example_user.songs.where(title: "主の足元に").first
   end
 
   def songs
