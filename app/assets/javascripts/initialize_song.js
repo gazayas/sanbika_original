9d6a83000d9fcc09e097a8f001afe75ac4f5bab7
gazayas <g-zayas@hotmail.com>
2018-03-30 15:04:07 +0900

    やっとできたと思います

--- app/assets/javascripts/initialize_song.js
+++ app/assets/javascripts/initialize_song.js
@@ -1,5 +1,8 @@
 /* songs/show.html.erbで使います */
 
+// TODO: print.html.erbで実装すること
+
+
 // TODO: sharp_or_flat_is_present_in(str)
 function replace_marks(str) {
   if (check_sharp(str)) {
@@ -11,44 +14,20 @@ function replace_marks(str) {
   return str;
 }
 
-function change_whitespace_of(str) {
-  str = str.replace(/&nbsp;/g, " ");
-  str = str.replace(/　/g, " "); // 全角を半角に
-  return str;
-}
-
-
-// TODO: Refactoring
-
 $(document).ready(function() {
-  // <br>で区切る
-  // 適切な正規表現を使ってコードが入ってるかどうか確認する
-    // 先頭には[A-G]がない場合、その行全体は歌詞だと判る
-  // 入ってたら、replace_marksして、<span class="chord"></span>で包む
-  // nameを与える
-  // joinして実装する
-
   var song_body_tag = document.getElementById("song_body");
-  song_body_tag.innerHTML = song_body_tag.innerHTML.replace(/<\/?p>/g, ""); // 厄介だな
-
-  // tinymceは最初の方に改行の\nを入れてしまうからそれを消す
-  song_body_tag.innerHTML = song_body_tag.innerHTML.replace(/\r?\n/, "");
-
-  var song_body = song_body_tag.innerHTML.split("<br>");
-  var regexp = /^[A-G]{1,1}/; //{1,1}をつけた方が確率を上げるかな
-  var chord_regexp = /^[A-G].*/;
+  var song_body = song_body_tag.innerHTML.split(/\r*\n/);
+  var chord_regexp = /[A-G]{1,1}/;
 
   for(var i = 0; i < song_body.length; i++) {
     var has_chords;
-
-    song_body[i] = change_whitespace_of(song_body[i]);
-    var str_ary = song_body[i].split(" ");
+    var str_ary = song_body[i].split(/(\s+|　+)/); // zenkakuも含めて
 
     for(var x = 0; x < str_ary.length; x++) {
-      if(str_ary[x] == "" || str_ary[x] == " ") {
-        // 空白だったらcontinue
-      } else if(!regexp.test(str_ary[x])) {
-        // falseが一回でもあったらbreak
+      if(/\s+|　+/.test(str_ary[x]) || str_ary[x] == "") {
+        // 空白だったら分からないからそのままcontinue
+      } else if(!chord_regexp.test(str_ary[x])) {
+        // falseが一回でも出たらbreak
         has_chords = false
         x = str_ary.length;
       } else {
@@ -58,18 +37,23 @@ $(document).ready(function() {
 
     if(has_chords) {
       for(x = 0; x < str_ary.length; x++) {
-        if(chord_regexp.test(str_ary[x])) {
-          console.log(RegExp.input);
-          str_ary[x] = '<span class="chord" name="' + replace_marks(RegExp.input) + '">' + replace_marks(RegExp.input) + '</span>';
+        if(/^\w/.test(str_ary[x])) {
+          str_ary[x] = 
+            '<span class="chord" name="' + replace_marks(str_ary[x]) + '">' +
+              replace_marks(str_ary[x]) +
+            '</span>';
         }
       }
     }
-    song_body[i] = str_ary.join(" ");
+
+    song_body[i] = str_ary.join("");
   }
 
   // joinして実装
-  song_body = song_body.join("<br/>");
-  song_body_tag.innerHTML = "<p>" + song_body + "</p>";
+  song_body = song_body.join("<br>");
+  song_body = song_body.replace(/^<br>\s{2,2}/, ""); // なぜこうなるだろうか分からんけど、<br>と空白が先頭に入るから消す
+  song_body_tag.innerHTML = song_body;
+
 
   // song.title_yomikataをtooltipとして表示します
   $(function () {
