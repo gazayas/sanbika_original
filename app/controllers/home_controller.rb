f946d16ee5f4a012607f4041c671c5dc48c2fd87
gazayas <g-zayas@hotmail.com>
2018-04-23 13:29:56 +0900

    Work on locales

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   def index
-    example_user = User.find_by(name: 'gazayas')
-    @example_song = example_user.songs.where(title: "主の足元に").first
+  	if I18n.locale == I18n.default_locale
+  		example_user = User.find_by(name: 'gazayas')
+    	@example_song = example_user.songs.where(title: "主の足元に").first
+    else
+    	# TODO: 英語のチャートと入れ替えてください。How Great Is Our God
+    	example_user = User.find_by(name: 'gazayas')
+    	@example_song = example_user.songs.where(title: "主の足元に").first
+    end
   end
 
   def songs
