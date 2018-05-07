6779654f84465f4054d0bb0d60b1af51fd0c0dfc
gazayas <g-zayas@hotmail.com>
2018-05-07 16:04:29 +0900

    ユーザ毎にsongのtitleをuniqueにしました

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ApplicationRecord
 
 	VIDEO_REGEXP = /\A(http)?(s)?(:\/\/)?(www\.youtube\.com\/watch\?v=)|(youtu\.be\/)(.*)\z/
 
-    validates :title, presence: true, length: { maximum: 50 }
+    validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
 	validates :artist, length: { maximum: 50 }
 	validates :key, presence: true, length: { maximum: 2 }
     validates :song_body, presence: true, length: { maximum: 7_000_000 }
@@ -12,6 +12,6 @@ class Song < ApplicationRecord
 	private
 
 	def check_link
-		errors.add(:video, '不正な値です') unless video == "" || !!video.match(VIDEO_REGEXP)
+		errors.add(:video, t(:invalid_link)) unless video == "" || !!video.match(VIDEO_REGEXP)
 	end
 end
