39b5a85c80a9d05b577b67e3f27b1469ef2522d0
gazayas <g-zayas@hotmail.com>
2018-05-07 15:05:09 +0900

    home#indexのexample_chartをロケールの方から読み込むことにしました

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	if I18n.locale == I18n.default_locale
-  		example_user = User.find_by(name: 'gazayas')
-    	@example_song = example_user.songs.where(title: "主の足元に").first
-    else
-    	# TODO: 英語のチャートと入れ替えてください。How Great Is Our God
-    	example_user = User.find_by(name: 'gazayas')
-    	@example_song = example_user.songs.where(title: "主の足元に").first
-    end
   end
 
   def songs
