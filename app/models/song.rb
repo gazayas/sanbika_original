e89fcc7b373fc6d716b88f1719e165bc9832d7d1
gazayas <g-zayas@hotmail.com>
2018-06-05 18:24:56 +0900

    謎の空白を消す

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,10 +3,10 @@ class Song < ApplicationRecord
 
 	VIDEO_REGEXP = /\A(http)?(s)?(:\/\/)?(www\.youtube\.com\/watch\?v=)|(youtu\.be\/)(.*)\z/
 
-    validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
+  validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
 	validates :artist, length: { maximum: 50 }
 	validates :key, presence: true, length: { maximum: 2 }
-    validates :song_body, presence: true, length: { maximum: 7_000_000 }
+  validates :song_body, presence: true, length: { maximum: 7_000_000 }
 	validates :video, :check_link, length: { maximum: 300 }
 
 	def self.search(search)
