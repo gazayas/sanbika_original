accf37e0554d0acd43f26a53fd2635a5be9556c2
gazayas <g-zayas@hotmail.com>
2018-06-12 09:33:26 +0900

    Fix spacing in model

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,20 +4,20 @@ class Song < ApplicationRecord
 
 	VIDEO_REGEXP = /\A(http)?(s)?(:\/\/)?(www\.youtube\.com\/watch\?v=)|(youtu\.be\/)(.*)\z/
 
-  validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
+ 	validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
 	validates :artist, length: { maximum: 50 }
 	validates :key, presence: true, length: { maximum: 2 }
-  validates :song_body, presence: true, length: { maximum: 7_000_000 }
+	validates :song_body, presence: true, length: { maximum: 7_000_000 }
 	validates :video, :check_link, length: { maximum: 300 }
 
 	#TODO: Upcaseかdowncase、どちらでも適切にマッチするように
 	def self.search(search)
-    if search
-      where(['title LIKE ?', "%#{search}%"]) #検索とtitleの部分一致を表示
-    else
-      all #全て表示
-    end
-  end
+		if search
+      		where(['title LIKE ?', "%#{search}%"]) #検索とtitleの部分一致を表示
+    	else
+    		all #全て表示
+    	end
+	end
 
 	private
 
