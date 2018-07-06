fcd8d98190efb44d24745c79330e0293329c7cec
gazayas <g-zayas@hotmail.com>
2018-07-06 10:55:41 +0900

    Change logic for home page search, remove Song model search method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,27 @@ class HomeController < ApplicationController
   end
 
   def songs
-    @songs = Song.page(params[:page]).search(params[:search]).per(25)
-
     # TODO: gemで漢字をひらながに変換してから順に並べたい
+    @songs = Song.all.page(params[:page]).per(25)
   end
 
   def search
+    #TODO: REFACTOOOOR
+
     @results = ""
 
-    case params[:search_filter]
-    when "Artist"
-      @results = Song.page(params[:page]).where(["artist LIKE ?", params[:search_contents]]).per(25)
-    when "Chart"
+    #TODO: Upcaseかdowncase、どちらでも適切にマッチするように
+    if params[:search_filter]
+      case params[:search_filter]
+      when "Artist"
+        @results = Song.page(params[:page]).where(["artist LIKE ?", params[:search_contents]]).per(25)
+      when "Chart"
+        @results = Song.page(params[:page]).where(["title LIKE ?", params[:search_contents]]).per(25)
+      when "User"
+        @results = User.page(params[:page]).where(["name LIKE ?", params[:search_contents]]).per(25)
+      end
+    else
       @results = Song.page(params[:page]).where(["title LIKE ?", params[:search_contents]]).per(25)
-    when "User"
-      @results = User.page(params[:page]).where(["name LIKE ?", params[:search_contents]]).per(25)
     end
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -9,15 +9,6 @@ class Song < ApplicationRecord
   validates :song_body, presence: true, length: { maximum: 7_000_000 }
   validates :video, :check_link, length: { maximum: 300 }
 
-  #TODO: Upcaseかdowncase、どちらでも適切にマッチするように
-  def self.search(search)
-    if search
-	  where(['title LIKE ?', "%#{search}%"]) #検索とtitleの部分一致を表示
-	else
-	  all #全て表示
-	end
-  end
-
   private
 
   def check_link
