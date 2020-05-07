9b90a1e0ec995c67338f64cdba76a63449595e92
gazayas <g-zayas@hotmail.com>
2020-05-07 12:20:32 +0900

    updates for rails 6

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :song do
-    title '歌です'
-    artist '歌手'
-    key 'C'
-    song_body '歌詞はここに入ります'
-    video 'https://youtu.be/itdgtFALCpo'
+    title {"歌です"}
+    artist {"歌手"}
+    key {"C"}
+    song_body {"歌詞はここに入ります"}
+    video {"https://youtu.be/itdgtFALCpo"}
   end
 end
