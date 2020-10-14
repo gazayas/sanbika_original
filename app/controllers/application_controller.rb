54f7e7835eedd4ddc3383c9392cb146201f5a858
gazayas <g-zayas@hotmail.com>
2020-10-14 21:47:37 +0900

    Add devise views and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+  end
+
+=begin
   def authenticate_current_user
     if params[:user_id]
       # その他関連付けられているモデルのコントローラの場合
@@ -15,6 +22,7 @@ class ApplicationController < ActionController::Base
       redirect_to user_path(@user), notice: t(:cant_change_info)
     end
   end
+=end
 
   private
 
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,27 @@
+class RegistrationsController < Devise::RegistrationsController	
+
+	# 要らないですかね。tokenをメールで私たら問題はないと思います。	
+	# またmailerについてはMichael Hartlさんが書いたe-bookを見てみたらいいかもしれません。	
+	# before_action :authenticate_current_user, only: [:edit]	
+
+	protected	
+	def update_resource(resource, params)	
+		resource.update_without_password(params)	
+	end	
+
+	# http://jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html	
+
+	private	
+
+	def sign_up_params	
+		params.require(:user).permit(:email, :password, :password_confirmation)	
+	end	
+
+	def sign_in_params	
+		params.require(:user).permit(:email, :password)
+	end	
+
+	def account_update_params	
+    	params.require(:user).permit(:email, :password, :password_confirmation)
+	end	
+end
