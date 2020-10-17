3cb61f6352126588bc263bf0a6b6f86ec0fd4c61
gazayas <g-zayas@hotmail.com>
2020-10-17 17:53:53 +0900

    Add style to omniauth links, etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  # I added the follwing three methods to enable
+  # the Devise partial on the home#index page
+  def devise_mapping
+    Devise.mappings[:user]
+  end
+
+  def resource_name
+    devise_mapping.name
+  end
+
+  def resource_class
+    devise_mapping.to
+  end
+
 end
