3c149690f775b3505a3b5ccce9357ee0ad0eabf9
gazayas <g-zayas@hotmail.com>
2021-04-13 14:27:11 +0900

    Adjust Song model tests

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :song do
-    title {"歌です"}
-    key {"C"}
-    song_body {"歌詞はここに入ります"}
-    video {"https://youtu.be/itdgtFALCpo"}
+    title  { '歌のタイトル' }
+    key { 'C' }
+    song_body { '歌詞はここに入ります' }
+    video { 'https://youtu.be/itdgtFALCpo' }
   end
 end
