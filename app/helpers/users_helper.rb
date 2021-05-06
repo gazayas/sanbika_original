2d7741265211893127d61319276eec703c1ce8e2
Gabriel Zayas <g-zayas@hotmail.com>
2021-05-06 22:18:38 +0900

    Edit users helper to display image properly

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,12 @@
 module UsersHelper
   def get_user_image
-    @user.user_image ? @user.user_image : 'user_black_logo.jpg'
+    # TODO: Check Facebook Developer's settings and see what can be done to fix this
+    if @user.user_image.present? && !!@user.user_image.match(/^http:/)
+      user_image = @user.user_image.gsub(/^http/, "https")
+    else
+      user_image = @user.user_image
+    end
+
+    @user.user_image ? user_image : 'user_black_logo.jpg'
   end
 end
